a0cf0a12115e5f9aaeb9b02c9ab4235dfaeb6e45
Marcel Goh <marcel.goh@mail.mcgill.ca>
2018-07-03 19:59:49 -0600

    added binary-search

---
+++ scheme/binary-search.scm
@@ -0,0 +1,19 @@
+; BINARY SEARCH: Written by Marcel Goh on 3 July 2018.
+
+(define (binary item list)        ; returns index of item in list
+  (define (nth n list)            ; returns element of list at index n
+    (if (= n 0) (car list) (nth (- n 1) (cdr list))))
+  (define (before n list)         ; returns sublist before index n
+    (if (= n 0) '() (cons (car list) (before (- n 1) (cdr list)))))
+  (define (after n list)          ; returns sublist after index n
+    (if (= n 0) (cdr list) (after (- n 1) (cdr list))))
+  (define (search item list add)  ; add is offset from start of original list
+    (let ((mid (floor (/ (length list) 2))))
+      (cond ((null? list) false)
+            ((= item (nth mid list))
+             (+ add mid))
+            ((< item (nth mid list))
+             (search item (before mid list) add))
+            ((> item (nth mid list))
+             (search item (after mid list) (+ add mid 1))))))
+  (search item list 0))
