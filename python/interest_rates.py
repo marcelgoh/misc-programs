50716bb80d904522f7f821b0745b4f1d88bda72b
Marcel Goh <marcel.goh@mail.mcgill.ca>
2018-07-18 20:16:52 -0600

    modified whitespace

--- python/interest_rates.py
+++ python/interest_rates.py
@@ -9,22 +9,22 @@ A collection of functions to calculate interest rates
 # given starting balance and monthly payment amount, returns end balance after given period
 def end_bal(balance, annual_interest, payment, months):
     for _ in range(months):
-        balance = balance + (annual_interest/12)*(balance-payment) - payment
+        balance = balance + (annual_interest/12)*(balance - payment) - payment
     return balance
 
 # returns total amount paid to interest over given period
 def sum_interest(balance, annual_interest, payment, months):
     acc = 0
     for _ in range(months):
-        acc += (annual_interest/12)*(balance-payment)
-        balance = balance + (annual_interest/12)*(balance-payment) - payment
+        acc += (annual_interest/12)*(balance - payment)
+        balance = balance + (annual_interest/12)*(balance - payment) - payment
     return acc
 
 # returns lowest monthly payment to pay off balance within given period
 def lowest_payment(balance, annual_interest, months):
     low = balance/months
-    high = balance * (1+annual_interest)**12/months
-    payment = (low + high)/2.0
+    high = balance*(1 + annual_interest)**12 / months
+    payment = (low + high) / 2.0
     while abs(end_bal(balance, annual_interest, payment, months)) > 0.01:
         if end_bal(balance, annual_interest, payment, months) > 0:
             low = payment
