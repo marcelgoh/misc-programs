1f57987ffb328a1764027dcbe3e407b4cac7f811
Marcel Goh <marcel.goh@mail.mcgill.ca>
2018-07-22 21:02:11 -0600

    solve seems to work

--- haskell/Sudoku/Solve.hs
+++ haskell/Sudoku/Solve.hs
@@ -1,4 +1,11 @@
-{- Sudoku solver program. WIP. Started by Marcel Goh on 22 July 2018 -}
+{- Sudoku solver program. Written by Marcel Goh on 22 July 2018 
+ - The program:
+ - 1. Asks for filename of a text file via standard input.
+ -   (File must contain at least 81 numbers)
+ - 2. Reads the first 81 numbers in the file and prints the puzzle grid.
+ -   (The puzzle is assumed to be solvable)
+ - 3. Solves the puzzle and prints the solution grid.
+ -}
 
 import Data.Char
 
@@ -8,11 +15,9 @@ createGrid s = aux s 81 []
     where
       aux s n lst =
         case n of
-          -- corrects reversed list before returning
           0 -> reverse lst
           _ -> case s of
                  c:cs -> if isDigit c
-                            -- constructs list in reverse order
                             then aux cs (n - 1) (digitToInt c : lst)
                             else aux cs n lst
 
@@ -26,19 +31,8 @@ stringGrid lst = addNewlines [] 0 $ reverse $ map intToDigit lst
         case input of
           []   -> output
           c:cs -> if counter `mod` 9 == 0 && counter /= 0
-                     -- constructs output string in reverse (i.e. correct) order
                      then addNewlines (c : '\n' : output) (counter + 1) cs
                      else addNewlines (c : ' ' : output) (counter + 1) cs
-
-blankIndices :: [Int] -> [Int]
-blankIndices = aux 0 []
-    where
-      aux counter indices grid =
-        case grid of
-          []     -> reverse indices
-          n:ns -> if n == 0
-                     then aux (counter + 1) (counter : indices) ns
-                     else aux (counter + 1) indices ns
 
 -- fills blanks in problem grid with elements from solution list
 fill :: [Int] -> [Int] -> [Int]
@@ -67,7 +61,7 @@ getBox :: Int -> [Int] -> [Int]
 getBox index grid =
     map snd $ filter (\x -> fst x `mod` 9 `div` 3 + fst x `div` 27 * 3 == index) $ zip [0..] grid
 
--- checks if putting num into cell is a safe operation
+-- checks if putting num into cell is a safe operation and returns boolean
 safe :: Int -> Int -> [Int] -> Bool
 safe cell num grid =
     let row = cell `div` 9
@@ -77,31 +71,35 @@ safe cell num grid =
             num `elem` getCol col grid ||
             num `elem` getBox box grid)
 
-   {-
+-- recursive backtrack algorithm: returns a list of solutions to fill blanks in problem grid
 solve :: [Int] -> [Int]
 solve grid =
     let blanks = filter (0 ==) grid
-        indices = blankIndices grid
-    in try indices [] blanks []
+        indicesOfBlanks = map fst $ filter (\x -> snd x == 0) $ zip [0..] grid
+    in try indicesOfBlanks [] blanks []
        where
          try unfilled filled blanks solution =
            case blanks of
              []   -> reverse solution
              n:ns -> if n < 9
-                        then let guess = 
-                    -}
+                        then let guess = n + 1
+                                 ansSoFar = reverse solution ++ blanks
+                                 cell = head unfilled
+                             in if safe cell guess $ fill grid ansSoFar
+                                   -- guess by pushing from unfilled to filled stacks and from 
+                                   -- blank stack to solution stack
+                                   then try (tail unfilled) (cell : filled) ns (guess : solution)
+                                   else try unfilled filled (guess : ns) solution
+                             -- backtrack: push from filled to unfilled and solution to blank
+                        else try (head filled : unfilled) (tail filled) (head solution : 0 : ns) (tail solution)
 
-main = do putStrLn "Enter name of file to load: "
+-- asks user for input and prints to terminal
+main = do putStrLn "Enter name of file to load:"
           filename <- getLine
           contents <- readFile filename
           let grid = createGrid contents
-          putStrLn "Problem grid: "
+          putStrLn "Problem grid:"
           putStrLn $ stringGrid grid
-          print $ show $ fill [1, 0, 3, 0, 0] [9, 9, 9]
-          print $ show $ blankIndices [1, 0, 3, 0, 0]
-          print $ show $ blankIndices [1, 1, 1]
-          print $ show $ getRow 2 grid
-          print $ show $ getCol 7 grid
-          print $ show $ getBox 4 grid
-          print $ show $ safe 4 4 grid
-          print $ show $ safe 13 3 grid
+          let solution = solve grid
+          putStrLn "Solution grid:"
+          putStrLn $ stringGrid $ fill grid solution
