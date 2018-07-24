32a30fa4d7b1b2bfe9fb41beebed7492056a4530
Marcel Goh <marcel.goh@mail.mcgill.ca>
2018-07-23 20:57:27 -0600

    added Sort

---
+++ haskell/Sort.hs
@@ -0,0 +1,31 @@
+{- Various sort algorithms. Written by Marcel Goh on 23 July 2018 -}
+
+-- actually a "pseudo-quicksort"
+quicksort :: [Int] -> [Int]
+quicksort []     = []
+quicksort (n:ns) = (quicksort $ filter (n >) ns) ++ (n : (quicksort $ filter (n <=) ns))
+
+-- mergesort
+mergesort :: [Int] -> [Int]
+mergesort =
+    let merge :: [Int] -> [Int] -> [Int]
+        merge ms []         = ms
+        merge [] ns         = ns
+        merge (m:ms) (n:ns) = if m <= n
+                                 then m : (merge ms (n:ns))
+                                 else n : (merge ns (m:ms))
+        sort :: [Int] -> [Int]
+        sort []  = []
+        sort [n] = [n]
+        sort ns =
+            let fsthalf = take (length ns `div` 2) ns
+                sndhalf = drop (length ns `div` 2) ns
+            in merge (sort fsthalf) (sort sndhalf)
+    in sort
+
+main = do
+    putStrLn "Enter numbers separated by spaces:"
+    numString <- getLine
+    let numList = map read $ words numString
+    print $ show $ quicksort numList
+    print $ show $ mergesort numList
