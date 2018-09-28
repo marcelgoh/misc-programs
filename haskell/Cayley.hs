076910e0d5e9dd60fd62f088a5ef4838d90370ed
Marcel Goh <marcel.goh@mail.mcgill.ca>
2018-09-28 12:46:35 -0400

    refactored with new datatype Group

--- haskell/Cayley.hs
+++ haskell/Cayley.hs
@@ -5,19 +5,25 @@
 import Data.List
 import Text.Printf
 
-data Element = Int Int| Char Char | String String
+data Element = Int Int | Char Char | String String
          deriving (Eq, Show)
+-- a group is a set and a table corresponding to it
+-- (we cannot define it as a set and operation because some operations are
+-- easily definable in code)
+data Group = Group [Element] [[Element]]
 
--- creates a table of Elements given a set of integers and a binary operation
--- only supports Ints
-fillCayley :: [Int] -> (Int -> Int -> Int) -> [[Element]]
-fillCayley set operation =
-  map (\l -> map Int l) $ map (\g -> map (\x -> operation g x) set) set
+-- creates a group given a set of integers and a binary operation
+groupFromOperation :: [Int] -> (Int -> Int -> Int) -> Group
+groupFromOperation ints operation =
+  let set = map Int ints
+      table = map (\l -> map Int l) $ map (\g -> map (\x -> operation g x) ints) ints
+  in Group set table
+
 
 -- given a set and Cayley table (a list of rows), prints the table in a readable format
 -- supports any kind of Element
-printCayley :: [Element] -> [[Element]] -> IO ()
-printCayley set table =
+printCayley :: Group -> IO ()
+printCayley (Group set table) =
   let display :: Element -> String
       display elem =
         case elem of
@@ -54,9 +60,9 @@ getCols rows =
           _    -> iter ((map head rowsLeft) : acc) (map tail rowsLeft)
   in iter [] rows
 
--- determines if given table corresponds to an abelian (i.e. commutative) group
-tableAbelian :: [[Element]] -> Bool
-tableAbelian rows =
+-- determines if given group is abelian
+abelian :: Group -> Bool
+abelian (Group _ rows) =
   let cols = getCols rows
       -- both tables must be of equal size (but all Cayley tables should be square anyway)
       tableEquals :: [[Element]] -> [[Element]] -> Bool
@@ -65,6 +71,8 @@ tableAbelian rows =
           [] -> True
           _  -> if head t1 /= head t2 then False else tableEquals (tail t1) (tail t2)
   in tableEquals rows cols
+
+-- checks if group contains identity element
 
 -- quaternion group for testing
 o = String "1"
@@ -86,12 +94,12 @@ quatTable = [[o, o', i, i', j, j', k, k'],
              [j', j, k, k', o, o', i', i],
              [k, k', j, j', i', i, o', o],
              [k', k, j', j, i, i', o, o']]
+quatGroup :: Group
+quatGroup = Group quatSet quatTable
 
 -- U(30) for testing
-thirtySet :: [Element]
-thirtySet = map Int [1, 7, 11, 13, 17, 19, 23, 29]
-thirtyTable :: [[Element]]
-thirtyTable = fillCayley [1,7,11,13,17,19,23,29] (\x y -> (x*y) `mod` 30)
+thirtyGroup :: Group
+thirtyGroup = groupFromOperation [1,7,11,13,17,19,23,29] (\x y -> (x*y) `mod` 30)
 
 -- dihedral groups D2, D3 for testing
 e = Char 'e'
@@ -108,6 +116,8 @@ rectangleTable = [[e,a,b,c],
                   [a,e,c,b],
                   [b,c,e,a],
                   [c,b,a,e]]
+rectangleGroup :: Group
+rectangleGroup = Group rectangleSet rectangleTable
 -- D3
 triangleSet :: [Element]
 triangleSet = [e, a, b, c, d, f]
@@ -118,3 +128,5 @@ triangleTable = [[e,a,b,c,d,f],
                  [c,d,f,e,a,b],
                  [d,c,a,b,f,e],
                  [f,b,c,a,e,d]]
+triangleGroup :: Group
+triangleGroup = Group triangleSet triangleTable
