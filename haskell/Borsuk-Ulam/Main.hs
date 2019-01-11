9ed85ebe2f2a8cebcb41ebeaf3e4810212e89306
Marcel Goh <marcel.goh@mail.mcgill.ca>
2019-01-11 13:22:55 -0500

    Added data-fetching functionality

--- haskell/Borsuk-Ulam/Main.hs
+++ haskell/Borsuk-Ulam/Main.hs
@@ -1,16 +1,25 @@
 -- Program to find two diametrically opposed points on the globe with 
 -- the same temperature. Written by Marcel Goh.
 
+import Data.Either
+import Network.Curl.Download
 import Text.Read
 
+import qualified Data.ByteString.Char8 as C
+
 -- A point on the globe is defined as a latitude, longitude, and temperature.
-data Point = Point Double Double Int
+data Point = Point Double Double Double
 
 handle :: String -> IO ()
 handle input =
     case readMaybe input :: Maybe Int of
       Just n -> case n of
-                  1 -> do putStrLn "Option 1 chosen."
+                  1 -> do putStrLn "Trying to find data from web..."
+                          doc <- openURI "https://aviationweather.gov/adds/dataserver_current/current/metars.cache.csv"
+                          case doc of
+                            (Left _)        -> putStrLn "Error writing file."
+                            (Right bytestr) -> do writeFile "./Current/data.csv" (C.unpack bytestr)
+                                                  putStrLn "Data fetched successfully from server."
                           main
                   2 -> do putStrLn "Option 2 chosen."
                           main
@@ -21,11 +30,10 @@ handle input =
                     main
 
 main :: IO ()
-main = do
-    putStrLn "****************************************"
-    putStrLn "Choose an option by entering a number "
-    putStrLn "1) Import new data"
-    putStrLn "2) Find antipodes with same temperature"
-    putStrLn "3) Exit the program"
-    input <- getLine
-    handle input
+main = do putStrLn "****************************************"
+          putStrLn "Choose an option by entering a number "
+          putStrLn "1) Update to current weather data"
+          putStrLn "2) Find antipodes with same temperature"
+          putStrLn "3) Exit the program"
+          input <- getLine
+          handle input
--- haskell/Borsuk-Ulam/readme.md
+++ haskell/Borsuk-Ulam/readme.md
@@ -3,4 +3,5 @@
 A small program that illustrates the Borsuk-Ulam theorem by finding two antipodal points on the globe with the same temperature.  
 
 ## Authors
+Data: [Aviation Weather Centre](https://aviationweather.gov) (National Oceanic and Atmospheric Administration)
 Code: Marcel Goh
