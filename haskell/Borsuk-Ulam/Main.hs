f7f13ff76221c4316cd01db419ec2809de4b3cfc
Marcel Goh <marcel.goh@mail.mcgill.ca>
2019-01-13 22:35:28 -0500

    Can find antipodes with least error now

--- haskell/Borsuk-Ulam/Main.hs
+++ haskell/Borsuk-Ulam/Main.hs
@@ -6,6 +6,7 @@ import Data.Either
 import Data.List
 import Data.List.Split
 import Network.Curl.Download
+import Text.Printf
 import Text.Read
 
 import qualified Data.ByteString.Char8 as C
@@ -44,6 +45,49 @@ getPoints strList =
                         _       -> iterate ss acc
     in sort (iterate strList [])
 
+-- restructures list to group coordinates with same temperature
+-- assumes point list is already sorted by temperature
+byTemp :: [Point] -> [([Coord], Double)]
+byTemp [] = []
+byTemp points =
+    let iterate pts acc (list, temp) =
+            case pts of
+              []             -> (list, temp):acc
+              (Point c t):ps -> if 0.01 > t - temp
+                                   then iterate ps acc (c:list, temp)
+                                   else iterate ps ((list, temp):acc) (c:[], t)
+    in iterate points [] ([], tempFromPoint (head points))
+
+-- converts coordinate list to antipode lists
+toAntipode :: [([Coord], Double)] -> [([Antipode], Double)]
+toAntipode tuples =
+        -- remove temperatures with only one location
+    let trimSingles :: [([Coord], Double)] -> [([Coord], Double)] -> [([Coord], Double)]
+        trimSingles tups acc =
+            case tups of
+              [] -> acc
+              ((list, t):rest) -> case list of
+                                    c:[] -> trimSingles rest acc
+                                    _    -> trimSingles rest ((list, t):acc)
+        convert :: ([Coord], Double) -> ([Antipode], Double)
+        convert (cs, d) = (antipodeList cs, d)
+    in map convert (trimSingles tuples [])
+
+minAntipode :: [([Antipode], Double)] -> [(Antipode, Double)]
+minAntipode tuples =
+    map (\(as, d) -> (minimum as, d)) tuples
+
+leastError :: [(Antipode, Double)] -> (Antipode, Double)
+leastError [] = error "Cannot call on empty list: LEAST_ERROR"
+leastError tuples =
+    foldr (\ (a1, d1) (a2, d2) -> if a1 < a2 then (a1, d1) else (a2, d2)) (head tuples) tuples
+
+printByTemp :: (Antipode, Double) -> IO ()
+printByTemp (a, t) =
+    do printf "{%.1f deg. C:\n" t
+       printAntipode a
+       putStrLn "}"
+
 -- handle different user inputs
 handle :: String -> IO ()
 handle input =
@@ -58,10 +102,16 @@ handle input =
                           main
                   2 -> do putStrLn "Analysing data on file..."
                           doc <- readFile "./Current/data.csv"
-                          let points = getPoints (lines (removeLines doc 6))
-                          mapM_ printPoint points
+                          let ants = toAntipode $ byTemp $ getPoints $ lines (removeLines doc 6)
+                          printByTemp $ leastError $ minAntipode ants
                           main
-                  3 -> do putStrLn "Closing program..."
+                  3 -> do putStrLn "Enter the relative path of a file"
+                          filename <- getLine
+                          doc <- readFile filename
+                          let ants = toAntipode $ byTemp $ getPoints $ lines (removeLines doc 6)
+                          printByTemp $ leastError $ minAntipode ants
+                          main
+                  4 -> do putStrLn "Closing program..."
                   _ -> do putStrLn "Not a valid option."
                           main
       Nothing -> do putStrLn "Not a valid option"
@@ -72,7 +122,8 @@ main :: IO ()
 main = do putStrLn "****************************************"
           putStrLn "Choose an option by entering a number "
           putStrLn "1) Update to current weather data"
-          putStrLn "2) Find antipodes with same temperature"
-          putStrLn "3) Exit the program"
+          putStrLn "2) Find antipodes in current weather data"
+          putStrLn "3) Import data from file"
+          putStrLn "4) Exit the program"
           input <- getLine
           handle input
--- haskell/Borsuk-Ulam/Point.hs
+++ haskell/Borsuk-Ulam/Point.hs
@@ -15,18 +15,24 @@ instance Ord Point where
 
 -- A pair of points consists of two points and an error denoting how 
 -- close they are to being antipodes.
-data Antipode = Antipode Point Point Double
+data Antipode = Antipode Coord Coord Double
 -- For sorting purposes we only care about the error
 instance Eq Antipode where
-    (Antipode p1 p2 e) == (Antipode q1 q2 f) = 0.01 > abs (e - f)
+    (Antipode c1 c2 e) == (Antipode d1 d2 f) = 0.01 > abs (e - f)
 instance Ord Antipode where
-    (Antipode p1 p2 e) `compare` (Antipode q1 q2 f) = e `compare` f
+    (Antipode c1 c2 e) `compare` (Antipode d1 d2 f) = e `compare` f
+
+-- get a point's coordinates
+coordFromPoint :: Point -> Coord
+coordFromPoint (Point c _) = c
+
+-- get a point's temperature
+tempFromPoint :: Point -> Double
+tempFromPoint (Point _ t) = t
 
 -- print a point to standard out
 printPoint :: Point -> IO ()
-printPoint p =
-    case p of
-      (Point (Coord lat long) temp) -> do printf "(%.4f, %.4f, %.4f)\n" lat long temp
+printPoint (Point (Coord lat long) temp) = do printf "(%.2f, %.2f, %.2f)\n" lat long temp
 
 -- get coordinates of opposite side of the world
 opposite :: Coord -> Coord
@@ -35,8 +41,8 @@ opposite (Coord lat long) =
 
 -- find distance between two coordinates in km, assuming a spherical globe
 -- uses the haversine formula
-dist :: Coord -> Coord -> Double
-dist (Coord lat1 long1) (Coord lat2 long2) =
+haversine :: Coord -> Coord -> Double
+haversine (Coord lat1 long1) (Coord lat2 long2) =
     let phi1 = lat1 * (pi/180)
         phi2 = lat2 * (pi/180)
         dphi = (lat2-lat1) * (pi/180)
@@ -45,7 +51,23 @@ dist (Coord lat1 long1) (Coord lat2 long2) =
         c = 2 * (atan2 (sqrt a) (sqrt (1 - a)))
     in 6371 * c
 
-here :: Coord
-here = (Coord 32.9697 (-96.80322))
-there :: Coord
-there = (Coord 29.46786 (-98.53506))
+makeAntipode :: Coord -> Coord -> Antipode
+makeAntipode c1 c2 = (Antipode c1 c2 (haversine (opposite c1) c2))
+
+-- constructs a list of antipodes (with their error) from a list of coordinates
+antipodeList :: [Coord] -> [Antipode]
+antipodeList coordinates =
+    let buildAcc c cs acc =
+            case cs of
+              []      -> acc
+              car:cdr -> buildAcc c cdr ((makeAntipode c car):acc)
+        iterate coords acc =
+            case coords of
+              []   -> acc
+              c:cs -> iterate cs (buildAcc c cs acc)
+    in iterate coordinates []
+
+-- print an antipode to standard out
+printAntipode :: Antipode -> IO ()
+printAntipode (Antipode (Coord lat1 long1) (Coord lat2 long2) err) =
+    do printf "(%.2f, %.2f) <=> (%.2f, %.2f), %.2f\n" lat1 long1 lat2 long2 err
